b2d063b9469736adaa1078869aa698c3c4d2bd24
Your Name <you@example.com>
2025-10-07 17:53:45 -0700

    start

---
+++ src/js/main.js
@@ -0,0 +1,35 @@
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+
+import Swiper from "swiper";
+import { Navigation, Pagination } from "swiper/modules";
+
+const swiper = new Swiper(".mySwiper", {
+  modules: [Navigation, Pagination],
+  slidesPerView: 3,
+  spaceBetween: 0,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    0: {
+      // mobil
+      slidesPerView: 2,
+    },
+    640: {
+      // tablet
+      slidesPerView: 3,
+    },
+    1024: {
+      // descktop
+      slidesPerView: 3,
+    },
+  },
+});
